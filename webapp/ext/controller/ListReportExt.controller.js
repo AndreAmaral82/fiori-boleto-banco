69300be28a34c1262e31f669fa2fe0a4156b910a
Claudio Silva <claudio.silva@itspoa.com>
2020-08-27 15:48:54 -0300

    app finalizado

---
+++ webapp/ext/controller/ListReportExt.controller.js
@@ -0,0 +1,5 @@
+sap.ui.controller("itsgrp.com.bancoboleto.ext.controller.ListReportExt", {
+
+	onClickActiongetzfit100_06Set1: function (oEvent) {},
+	onClickActiongetzfit100_06Set2: function (oEvent) {}
+});
